5edae5476f3a22f58a69457866060ddf2a865953
Logan Madej <lnmadej23@gmail.com>
2021-09-20 09:24:40 -0400

    Add project files.

---
+++ Creating Class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_Class
+{
+
+    public class Person
+    {
+        public string CharName;
+        public int CharAge;
+        public string CharWeapon;
+
+        public void Greeting()
+        {
+            Console.WriteLine("You have gone on a journey, " + CharName + ". You are only " + CharAge + " and your only friend is a " + CharWeapon);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.CharName = "SlowerthanMOST99";
+            person.CharAge = 17;
+            person.CharWeapon = "Mace";
+
+            person.Greeting();
+            Console.ReadKey();
+        }
+    }
+}
